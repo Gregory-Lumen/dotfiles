6e392f2aab039f1731b2f6f93122141f0b99588b
Gregory Lumen (he/him) <gregorylumen@microsoft.com>
2024-10-04 17:31:48 +0000

    Streamlining python setup

--- install-apt-packages.sh
+++ install-apt-packages.sh
@@ -1,7 +1,13 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+
 sudo apt-get install -y \
     gnome-keyring \
     chrpath diffstat zstd \
-    ninja-build
+    ninja-build \
+    python3.12
 
+sudo ln -fs /usr/bin/python3.12 /usr/bin/python3
 
-pip install ranger-fm poetry
+python3 -m ensurepip --upgrade
+pip3 install ranger-fm poetry
